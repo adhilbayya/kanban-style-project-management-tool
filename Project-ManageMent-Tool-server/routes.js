b8b4d9759359eff1f4cf8a5e15983be144d73897
Adhil Mohammed <adhilvk445@gmail.com>
2025-10-21 15:27:35 +0530

    Added the routes for the update and delete

--- Project-ManageMent-Tool-server/routes.js
+++ Project-ManageMent-Tool-server/routes.js
@@ -6,6 +6,8 @@ const dbController = require("./dbController");
 router.get("/projects", dbController.getAllProjects);
 router.post("/projects", dbController.createProject);
 router.get("/projects/:projectId", dbController.getProjectById);
+router.put("/projects/:projectId", dbController.updateProject);
+router.put("/projects/:projectId", dbController.deleteProject);
 
 // Card routes (project-specific)
 router.get("/projects/:projectId/cards", dbController.getCardsByProject);
